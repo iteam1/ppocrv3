40a192d1b1231a3f5509f9a802fa2b1a83ce72f5
locchuong <locchuong9@gmail.com>
2023-06-09 11:04:02 +0700

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,19 @@
 # setup environment, packages, etc.
 #!usr/bin/bash
 
-pip install -r PaddleOCR/requirements.txt
+# setup python 3.8
+python3 --version
+
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1
+
+sudo update-alternatives --config python3
+
+python3 --version
+
+sudo apt install python3-pip
+
+# pip install -r PaddleOCR/requirements.txt
+
 pip install paddlepaddle-gpu
+
 pip install paddlepaddle
